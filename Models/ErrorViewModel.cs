dcd16b4216e0181cf9bb9e89df60f980597fa50d
melihoz13 <10487163@ucn.dk>
2024-09-08 18:00:33 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GrandesRentACar.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
